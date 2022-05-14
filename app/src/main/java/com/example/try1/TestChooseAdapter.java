22d7e64be0de63bbbeb22732751c1aaaf517d314
lenaasayed <lenaa.sayed7@gmail.com>
2022-05-14 20:22:40 +0200

    train an test

---
+++ app/src/main/java/com/example/try1/TestChooseAdapter.java
@@ -0,0 +1,68 @@
+package com.example.try1;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TestChooseAdapter extends RecyclerView.Adapter<TestChooseAdapter.ViewHolder>{
+    private Item[] listdata;
+    Context cont;
+
+    public TestChooseAdapter(Item[] listdata) {
+        this.listdata = listdata;
+    }
+    @Override
+    public TestChooseAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        cont=parent.getContext();
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View listItem= layoutInflater.inflate(R.layout.mainitemlayout, parent, false);
+        TestChooseAdapter.ViewHolder viewHolder = new TestChooseAdapter.ViewHolder(listItem);
+        return viewHolder;
+    }
+    @Override
+    public void onBindViewHolder(final TestChooseAdapter.ViewHolder holder, @SuppressLint("RecyclerView") final int position) {
+        holder.imageView.setImageResource(listdata[position].getImage());
+        final String name=listdata[position].getName();
+
+        holder.imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+           public void onClick(View view) {
+//                Animation animation= AnimationUtils.loadAnimation(cont,R.anim.blink_anim);
+//                holder.imageView.startAnimation(animation);
+//                  holder.txt.setText("aaaaaaaaaaaaa");
+                Intent intent;
+                String key="answer";
+                intent =  new Intent(cont, TestChoose.class);
+                intent.putExtra(key, position);
+                cont.startActivity(intent);
+
+            }
+        });
+
+    }
+    @Override
+    public int getItemCount() {
+        return listdata.length;
+    }
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView imageView;
+//        public TextView txt;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            this.imageView = (ImageView) itemView.findViewById(R.id.mainimageView);
+//            this.txt=(TextView) itemView.findViewById(R.id.testtext);
+
+        }
+    }
+
+}

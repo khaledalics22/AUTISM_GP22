ddaa499bb5f6848470e149e9104cca9dfa19eb49
Aya Samir Abohadima <48763545+ayaabohadima@users.noreply.github.com>
2022-07-15 23:11:14 +0200

    Delete src directory

--- src/auth/dto/login.dto.ts
+++
@@ -1,9 +0,0 @@
-import { Length, IsEmail, IsString, IsOptional } from "class-validator";
-export class LoginDto {
-  @IsString()
-  password: string;
-  @IsString()
-  @IsOptional()
-  identifier: string;
-}
-
